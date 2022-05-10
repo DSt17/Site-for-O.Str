db9914391db489996654b9e36efd2d870b97a55d
DSt17 <dima3771770@gmail.com>
2022-05-10 16:16:45 +0300

    compl contacts box

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from "./header/Header";
 import Main from "./main/Main";
 import Contacts from "./contacts/Contacts";
 import Footer from "./footer/Footer";
+import Nav from "./nav/Nav";
+import React from "react";
 
 function App() {
     return (
